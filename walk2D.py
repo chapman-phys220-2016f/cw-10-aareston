1e06b1df909f5fc3f0489e3f4a7c8c00fc5de54e
Aaron Grisez <grise100@mail.chapman.edu>
2016-11-04 01:09:01 +0000

    added random walk

--- walk2D.py
+++ walk2D.py
@@ -11,5 +11,43 @@ def walk(points, steps, plot_step):
     """
     Generates random walks for a given number of particles in a given number of steps. The steps are represented 
     """
-    xpositions = np.zeros(points)
+    xpositions = np.zeros(points)# These 2 lines generate a 2 by points array representing final positions for each point after the random walk
     ypositions = np.zeros(points)
+    position_axes = [xymin, xymax, xymin, xymax]
+    moves = np.random.random_integers(1, 4, size = points * steps)
+    moves.shape = (steps, points)# Reshape random moves matrix so we have $steps rows and $points columns, each row vector is the moves for a single particle in the whole walk
+
+    xymax = 3 * np.sqrt(steps)
+    xymin = -xymax
+
+    NORTH = 1
+    SOUTH = 2
+    EAST = 3
+    WEST = 4
+
+    for step in range(steps):
+        this_move = moves[step, :]# Selects the row vector which represents one step for each particle in the walk
+        ypositions += np.where(this_move == NORTH, 1, 0)
+        ypositions -= np.where(this_move == SOUTH, 1, 0)
+        xpositions += np.where(this_move == EAST, 1, 0)
+        xpositions -= np.where(this_move == WEST, 1, 0)# These four lines update the array of particle positions based on what the current move was for each particle
+
+
+        if (step + 1) % plot_step == 0;
+            plot_title = '%d particles after %d steps' % (particles, step + 1)
+            file_name = 'tmp_%03d.pdf' % (step + 1)
+            plt.plot(xpositions, ypositions, 'ko', axis = position_axes, title = plot_title, savefig = file_name)
+
+def main(points, steps, plot_step):
+    import datetime as dt
+    time = dt.datetime.now()
+    np.random.seed(time.microsecond)
+    x, y = walk(points, steps, plot_step)
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    points = int(args[1])
+    steps = int(args[2])
+    plot_step = int(args[3])
+    main(points, steps, plot_step)
