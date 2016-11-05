6dbcf591f0a05ce7a857c8248d3b167a936ba1db
Aaron Grisez <grise100@mail.chapman.edu>
2016-11-05 21:11:57 +0000

    Working code

--- walk2D.py
+++ walk2D.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import numpy as np
-
+import matplotlib as mtplt
+mtplt.use('Agg')
+import matplotlib.pyplot as plt
+import shutil as sh
 
 """
 Implements an ensemble of random walks and returns the endpoint of each
@@ -9,16 +12,14 @@ Implements an ensemble of random walks and returns the endpoint of each
 
 def walk(points, steps, plot_step):
     """
-    Generates random walks for a given number of particles in a given number of steps. The steps are represented 
+    Generates random walks for a given number of particles in a given number of steps. The steps are represented
     """
     xpositions = np.zeros(points)# These 2 lines generate a 2 by points array representing final positions for each point after the random walk
     ypositions = np.zeros(points)
-    position_axes = [xymin, xymax, xymin, xymax]
-    moves = np.random.random_integers(1, 4, size = points * steps)
-    moves.shape = (steps, points)# Reshape random moves matrix so we have $steps rows and $points columns, each row vector is the moves for a single particle in the whole walk
-
     xymax = 3 * np.sqrt(steps)
     xymin = -xymax
+    moves = np.random.random_integers(1, 4, size = points * steps)
+    moves.shape = (steps, points)# Reshape random moves matrix so we have $steps rows and $points columns, each row vector is the moves for a single particle in the whole walk
 
     NORTH = 1
     SOUTH = 2
@@ -33,16 +34,21 @@ def walk(points, steps, plot_step):
         xpositions -= np.where(this_move == WEST, 1, 0)# These four lines update the array of particle positions based on what the current move was for each particle
 
 
-        if (step + 1) % plot_step == 0;
-            plot_title = '%d particles after %d steps' % (particles, step + 1)
-            file_name = 'tmp_%03d.pdf' % (step + 1)
-            plt.plot(xpositions, ypositions, 'ko', axis = position_axes, title = plot_title, savefig = file_name)
+        if (step + 1) % plot_step == 0:
+            plot_title = '%d particles after %d steps' % (points, step + 1)
+            file_name = 'tmp_%03d.png' % (step + 1)
+            plt.title(plot_title)
+            plt.xlim([xymin,xymax])
+            plt.ylim([xymin,xymax])
+            plt.plot(xpositions, ypositions, 'ko')
+            plt.savefig(file_name)
+            plt.clf()
 
 def main(points, steps, plot_step):
     import datetime as dt
     time = dt.datetime.now()
     np.random.seed(time.microsecond)
-    x, y = walk(points, steps, plot_step)
+    x = walk(points, steps, plot_step)
 
 if __name__ == "__main__":
     import sys
